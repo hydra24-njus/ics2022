73d9222018af481cffce327400a6f40dc3254454
tracer-ics2021 <tracer@njuics.org>
2021-09-06 19:47:04 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a9427bbad87a43dfab664a0224beb487   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:47:04 up  1:08,  1 user,  load average: 0.53, 0.39, 0.45

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -73,7 +73,7 @@ static int cmd_x(char *args) {
   
   for(int i=0;i<n;i++){
   printf("0x%08x\t",vaddr_read(addr+i*4,4));
-  printf("0x %02x %02x %02x %02x",vaddr_read(addr+i*4+3,1),vaddr_read(addr+i*4+2,1),vaddr_read(addr+i*4+1,1),vaddr_read(addr+i*4,1));
+  printf("0x %02x %02x %02x %02x\n",vaddr_read(addr+i*4+3,1),vaddr_read(addr+i*4+2,1),vaddr_read(addr+i*4+1,1),vaddr_read(addr+i*4,1));
   }
   return 0;
 }
