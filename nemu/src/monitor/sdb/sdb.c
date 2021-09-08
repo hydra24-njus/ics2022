1d68d556bc407df20645403ec7107615c0f98890
tracer-ics2021 <tracer@njuics.org>
2021-09-08 19:41:50 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: d8ffcd40fb5940b7b76688ca390084c4   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:41:50 up 5 min,  1 user,  load average: 1.31, 0.78, 0.34

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -35,7 +35,7 @@ static int cmd_c(char *args) {
 }
 static int cmd_help(char *args);
 static int cmd_q(char *args) {
-  nemu_state.state = NEMU_QUIT;
+  //nemu_state.state = NEMU_QUIT;
   return -1;
 }
 
