652a8b4dacf2b3fb2823a4ed7270539b153089a4
tracer-ics2021 <tracer@njuics.org>
2021-09-10 00:10:18 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: eb0f65871fb1457aa6caeedd79092173   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  00:10:19 up 23 min,  1 user,  load average: 0.73, 0.42, 0.26

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -3,6 +3,8 @@
 NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
+  int test_1=nemu_state.state;
+  printf("%d\n",test_1);
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
   return !good;
