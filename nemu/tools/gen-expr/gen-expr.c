5f2115c2d81b6947d218359713ae32f54352b5ac
tracer-ics2021 <tracer@njuics.org>
2021-09-30 23:22:51 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 746a14ec196b4bcbbb075e21d1e46e2e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  23:22:52 up 13 min,  1 user,  load average: 0.20, 0.21, 0.14

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -15,10 +15,46 @@ static char *code_format =
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
-
-static void gen_rand_expr() {
-  buf[0] = '\0';
+int cnt=0;
+int choose(int n){
+  return rand()%n;
 }
+static void gen(char s){
+  buf[cnt]=s;
+  cnt++;
+  buf[cnt]='\0';
+}
+void gen_num(){
+  switch(choose(10)){
+    case 0:gen('0');break;
+    case 1:gen('1');break;
+    case 2:gen('2');break;
+    case 3:gen('3');break;
+    case 4:gen('4');break;
+    case 5:gen('5');break;
+    case 6:gen('6');break;
+    case 7:gen('7');break;
+    case 8:gen('8');break;
+    case 9:gen('9');break;
+  }
+}
+void gen_rand_op(){
+    switch(choose(3)){
+    case 0:gen('+');break;
+    case 1:gen('-');break;
+    case 2:gen('*');break;
+    //case 3:gen('3');break;
+
+  }
+}
+void gen_rand_expr() {
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
+}
+
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
@@ -29,6 +65,8 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    cnt=0;
+    memset(buf,0,sizeof(buf));
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
