9dc853f47e437d763a4741fad8988b6059506b12
tracer-ics2021 <tracer@njuics.org>
2021-10-01 11:03:14 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 7e981a9f54bd44638e85d78d9d0ed661   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  11:03:14 up 25 min,  1 user,  load average: 0.52, 0.60, 0.39

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,5 +1,22 @@
 #include <common.h>
 
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+char *ReadData(FILE *fp, char *buf)
+{
+return fgets(buf, 35399, fp);//读取一行到buf
+}
+void someprocess(char *buf)
+{
+printf("%s\n", buf);//这里的操作你自己定义
+}
+
+
+
 void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
@@ -12,6 +29,18 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
+
+char buf[35399], *p;
+FILE *fp =fopen("/home/ics2021/nemu/tools/gen-expr/input","r");
+
+while (1) {
+p = ReadData(fp, buf);//每次调用文件指针fp会自动后移一行
+if (!p)//文件读取结束则跳出循环
+break;
+someprocess(buf);
+}
+
+
 
   /* Start engine. */
   engine_start();
