901d05d137a33bc61f92d11105a10fe141c2dbbe
tracer-ics2021 <tracer@njuics.org>
2021-10-01 11:42:52 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 7e981a9f54bd44638e85d78d9d0ed661   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  11:42:53 up  1:05,  1 user,  load average: 0.65, 0.45, 0.51

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -280,12 +280,12 @@ word_t expr(char *e, bool *success) {
     return 0;
   }
   for (int i = 0; i < nr_token; i ++) {
-  if (tokens[i].type == '-' && (i == 0 || (tokens[i - 1].type !=TK_NUM&& tokens[i - 1].type ==TK_HEX&& tokens[i - 1].type ==TK_REG
-                                       && tokens[i - 1].type ==')')) ) {
+  if (tokens[i].type == '-' && (i == 0 || (tokens[i - 1].type !=TK_NUM&& tokens[i - 1].type !=TK_HEX&& tokens[i - 1].type !=TK_REG
+                                       && tokens[i - 1].type !=')')) ) {
     tokens[i].type = TK_NEG;
   }
-  if (tokens[i].type == '*' && (i == 0 || (tokens[i - 1].type !=TK_NUM&& tokens[i - 1].type ==TK_HEX&& tokens[i - 1].type ==TK_REG
-                                       && tokens[i - 1].type ==')')) ) {
+  if (tokens[i].type == '*' && (i == 0 || (tokens[i - 1].type !=TK_NUM&& tokens[i - 1].type !=TK_HEX&& tokens[i - 1].type !=TK_REG
+                                       && tokens[i - 1].type !=')')) ) {
     tokens[i].type = TK_DEREF;
   }
 }
