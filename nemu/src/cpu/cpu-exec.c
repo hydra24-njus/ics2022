420cf9db2c388fe2eb6bbe0095022cab742ab048
tracer-ics2021 <tracer@njuics.org>
2021-10-03 02:34:48 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 0a92e10b01284a2aa592ef331c6364f8   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  02:34:48 up 40 min,  1 user,  load average: 0.87, 0.64, 0.48

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -20,18 +20,20 @@ rtlreg_t tmp_reg[4];
 
 void device_update();
 void fetch_decode(Decode *s, vaddr_t pc);
-#ifdef CONFIG_WATCHPOINT 
+
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) log_write("%s\n", _this->logbuf);
 #endif
+#ifdef CONFIG_CONFIG_WATCHPOINT
   if(check_wp()==false){
     nemu_state.state=NEMU_STOP;
-  }\
+  }
+#endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 }
-#endif
+
 
 #include <isa-exec.h>
 
@@ -104,9 +106,7 @@ void cpu_exec(uint64_t n) {
   for (;n > 0; n --) {
     fetch_decode_exec_updatepc(&s);
     g_nr_guest_instr ++;
-    #ifdef CONFIG_WATCHPOINT
     trace_and_difftest(&s, cpu.pc);
-    #endif
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
