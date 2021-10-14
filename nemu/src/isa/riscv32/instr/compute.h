00a07a343343beb118de4987d1a5a7dea9eeaa3c
tracer-ics2021 <tracer@njuics.org>
2021-10-14 18:28:59 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: f39564ec09f047c88c02a228cd419d48   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:28:59 up 23 min,  1 user,  load average: 0.94, 0.94, 0.76

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -8,12 +8,12 @@ def_EHelper(sub){
   rtl_sub(s,ddest,id_src1->preg,id_src2->preg);
 }
 def_EHelper(addi){
-  rtl_addi(s,ddest,id_src1->preg,/*id_src2->imm*/1);
+  rtl_addi(s,ddest,id_src1->preg,id_src2->imm);
 }
 def_EHelper(sltiu){
   rtl_subi(s,t0,id_src1->preg,id_src2->imm);
   rtl_srli(s,t0,t0,31);
-  rtl_addi(s,ddest,t0,1);
+  rtl_addi(s,ddest,t0,0);
 }
 def_EHelper(auipc){
   rtl_addi(s,ddest,&cpu.pc,id_src1->imm);
