3ab10d3d8320e00a9d48a048ba06d0fbd8bc42ae
201240105-Zhai Jingbo <201240105@smail.nju.edu.cn>
2021-10-14 17:22:16 +0800

    modified makefile "make $$$ sdb"

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -12,7 +12,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -b -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
-SDBFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+SDBFLAGS  += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
