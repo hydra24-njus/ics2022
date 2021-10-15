57154e168633ef6989523e124d1a6ce03a9d8f54
tracer-ics2021 <tracer@njuics.org>
2021-10-16 02:14:04 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 0497217695ac4d0ba40e34ec5e7e58e1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  02:14:04 up  4:40,  1 user,  load average: 1.01, 0.96, 1.01

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,8 +1,8 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(add) f(sub) f(slt) f(sltu) f(xor) f(or) f(slli)\
-f(addi) f(sltiu) f(auipc) f(andi)\
+#define INSTR_LIST(f) f(add) f(sub) f(slt) f(sltu) f(xor) f(or) f(sll)\
+f(addi) f(sltiu) f(auipc) f(andi) f(slli)\
 f(jal) f(jalr)\
 f(lui) f(lw) f(lh) f(lhu) f(lb) f(lbu)\
 f(sw) f(inv)\
