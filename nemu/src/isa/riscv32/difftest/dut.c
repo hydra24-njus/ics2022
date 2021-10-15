49419f3467d1008b1597d772a99afa07a0edc371
tracer-ics2021 <tracer@njuics.org>
2021-10-16 02:26:18 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 0497217695ac4d0ba40e34ec5e7e58e1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  02:26:18 up  4:52,  1 user,  load average: 0.89, 0.94, 0.99

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -5,6 +5,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; i++) {
     if (ref_r->gpr[i]._32 != gpr(i)) {
+      printf("%d\nrefgpr=%u\tnemugpr=%u\n",i,ref_r->gpr[i]._32,gpr(i));
       return false;
     }
   }
