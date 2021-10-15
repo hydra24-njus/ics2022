7456ff57dfa4eaf8623e932e7d3f79851534bd9a
tracer-ics2021 <tracer@njuics.org>
2021-10-15 22:56:33 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 0497217695ac4d0ba40e34ec5e7e58e1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:56:33 up  1:22,  1 user,  load average: 2.66, 2.60, 2.70

--- nemu/src/isa/riscv32/instr/decode.c
+++ nemu/src/isa/riscv32/instr/decode.c
@@ -48,18 +48,10 @@ static def_DHelper(R) {
 
 static def_DHelper(J) {
   decode_op_r(s, id_dest, s->isa.instr.j.rd, true);
-  *t0=(s->isa.instr.j.simm20 << 20) | (s->isa.instr.j.imm19_12 << 12) 
-   | (s->isa.instr.j.imm11_ << 11) | (s->isa.instr.j.imm10_1  << 1);
-  decode_op_i(s,id_src1,*t0,false);
+  sword_t offset=(s->isa.instr.j.simm20 << 20) | (s->isa.instr.j.imm19_12 << 12) 
+               | (s->isa.instr.j.imm11_ << 11) | (s->isa.instr.j.imm10_1  << 1);
+  decode_op_i(s,id_src2,offset,false);
 }
-
-/*static def_DHelper(B) {
-  decode_op_r(s, id_src1, s->isa.instr.b.rs1, false);
-  decode_op_r(s, id_src2, s->isa.instr.b.rs2, false);
-  t0=(s->isa.instr.b.simm12 << 12) | (s->isa.instr.b.imm11 << 11) 
-   | (s->isa.instr.b.imm10_5 << 5) | (s->isa.instr.b.imm4_1 << 1);
-  decode_op_i(s,id_dest,t0,false);
-}*/
 
 def_THelper(load) {
   def_INSTR_TAB("??????? ????? ????? 010 ????? ????? ??", lw);
