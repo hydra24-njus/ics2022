409e1ceea637c1f33f9d930a5b39ef2e69938c7c
tracer-ics2021 <tracer@njuics.org>
2021-10-16 02:35:05 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 0497217695ac4d0ba40e34ec5e7e58e1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  02:35:05 up  5:01,  1 user,  load average: 1.19, 1.14, 1.06

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -38,7 +38,8 @@ static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- zeroext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  //TODO();
+  
 }
 
 static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {
