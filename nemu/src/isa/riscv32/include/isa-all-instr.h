04d04b0d5d8921d6a1a7ceb20a7a5e493bad649b
tracer-ics2021 <tracer@njuics.org>
2021-10-16 01:57:33 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 0497217695ac4d0ba40e34ec5e7e58e1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  01:57:33 up  4:23,  1 user,  load average: 1.12, 1.14, 1.23

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -4,7 +4,7 @@
 #define INSTR_LIST(f) f(add) f(sub) f(slt) f(sltu) f(xor) f(or)\
 f(addi) f(sltiu) f(auipc) f(andi)\
 f(jal) f(jalr)\
-f(lui) f(lw) f(lh)\
+f(lui) f(lw) f(lh) f(lhu)\
 f(sw) f(inv)\
 f(beq) f(bne) f(bge) f(bgeu) f(blt) f(bltu)\
 f(nemu_trap)
--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -5,6 +5,9 @@ def_EHelper(lh) {
   rtl_lm(s, ddest, dsrc1, id_src2->imm, 2);
   rtl_sext(s,ddest,ddest,16);
 }
+def_EHelper(lhu) {
+  rtl_lm(s, ddest, dsrc1, id_src2->imm, 2);
+}
 def_EHelper(sw) {
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
 }
