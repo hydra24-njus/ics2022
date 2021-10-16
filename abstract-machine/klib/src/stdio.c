2edb6612d363e366e877dd5d011e3d9117f681c9
tracer-ics2021 <tracer@njuics.org>
2021-10-16 22:14:37 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 9440892518994a0382050ac83d1396b2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  22:14:37 up 54 min,  1 user,  load average: 0.29, 0.43, 0.45

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
