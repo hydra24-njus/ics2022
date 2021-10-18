9aea9539206974a8372577343b337e3e4fb38c40
tracer-ics2021 <tracer@njuics.org>
2021-10-18 15:02:44 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 738cea58b3e2435681ca0a1c081c4c7e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  15:02:44 up  1:21,  1 user,  load average: 0.45, 0.51, 0.49

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,23 +2,28 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
-
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
 //参考了stdio库函数
+
+
+//vsprintf需要的结构体，包括一个指针和str长度。
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
+//使用参数列表fmt发送格式化输出到out
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
-
+//发送指定内容到out所指的字符串
 int sprintf(char *out, const char *fmt, ...) {
   va_list args;
 	int val;
-	
+	//这里对参数进行解构，将输出存入out在sprintf中完成。
 	va_start(args, fmt);
-	val = vsprintf(out, fmt, args);
+	val = vsprintf(out, fmt, args);//to be completed
 	va_end(args);
 
 	return val;
