48575d4658759f1d4e56f5efb366297fc3a926de
tracer-ics2021 <tracer@njuics.org>
2021-10-25 18:50:14 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 32de5c1ba33045deb580bc6a9dd7286a   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:50:14 up 17 min,  1 user,  load average: 0.63, 0.85, 0.57

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,24 +1,13 @@
 #include <am.h>
 #include <nemu.h>
 
-static uint64_t boot_time = 0;
-
-#define CLINT_MMIO 0x2000000ul
-#define TIME_BASE 0xbff8
-
-static uint64_t read_time() {
-  uint32_t lo = *(volatile uint32_t *)(CLINT_MMIO + TIME_BASE + 0);
-  uint32_t hi = *(volatile uint32_t *)(CLINT_MMIO + TIME_BASE + 4);
-  uint64_t time = ((uint64_t)hi << 32) | lo;
-  return time / 10;
-}
 
 void __am_timer_init() {
-  boot_time = read_time();
+  return;
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = read_time() - boot_time;
+  uptime->us = 0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
