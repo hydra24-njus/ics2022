6b501b9e245d6720ef99b6334374ad989dd825c9
tracer-ics2021 <tracer@njuics.org>
2021-10-27 12:13:12 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 7578d96472e643bcb8c315d77dc3e28f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  12:13:12 up  1:05,  1 user,  load average: 0.54, 0.43, 0.44

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t tmp=inl(KBD_ADDR);
+  kbd->keydown=(tmp&KEYDOWN_MASK)?true:false;
+  kbd->keycode=(kbd->keydown)?tmp-KEYDOWN_MASK:tmp;
 }
