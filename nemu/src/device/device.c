26277e4c6fbe378545e2341f76b6832e2d0ac19d
tracer-ics2021 <tracer@njuics.org>
2021-10-27 11:51:51 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 7578d96472e643bcb8c315d77dc3e28f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  11:51:51 up 44 min,  1 user,  load average: 0.43, 0.29, 0.47

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -19,12 +19,12 @@ void send_key(uint8_t, bool);
 void vga_update_screen();
 
 void device_update() {
-  static uint64_t last = 0;
+  /*static uint64_t last = 0;
   uint64_t now = get_time();
   if (now - last < 1000000 / TIMER_HZ) {
     return;
   }
-  last = now;
+  last = now;*/
 
   IFDEF(CONFIG_HAS_VGA, vga_update_screen());
 
