d32d34db2c7b2da83b0bb3a8ddd794f1a1cea274
tracer-ics2021 <tracer@njuics.org>
2021-10-27 13:20:38 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: e095e89d7ce14d0a9237e0bd52d2a0dd   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  13:20:38 up 17 min,  1 user,  load average: 0.59, 0.44, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,11 +2,14 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define W 400
+#define H 300
+
 
 void __am_gpu_init(){
   int i;
-  int w = 400; // TODO: get the correct width
-  int h = 300; // TODO: get the correct height
+  int w = W; // TODO: get the correct width
+  int h = H; // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i++)fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -21,6 +24,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb=(uint32_t*)FB_ADDR;
+  int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
+  uint32_t *pixel=ctl->pixels;
+  for(int i=0;i<h;i++){
+    for(int j=0;j<w;j++){
+      fb[(y+i)*W+x+j]=pixel[w*i+j];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
