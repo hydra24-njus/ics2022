8b5fea0dfcf903d04c55f7b1ae011f8fd998bd8a
tracer-ics2021 <tracer@njuics.org>
2021-10-28 11:26:36 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 0a302faeda734cdb91c404e72829463a   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  11:26:36 up  1:18,  1 user,  load average: 1.25, 0.88, 0.74

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -8,7 +8,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = (inl(RTC_ADDR+4)-bootime_hi)*50000+inl(RTC_ADDR)-bootime_lo;
+  uptime->us = (inl(RTC_ADDR+4)-bootime_hi)*1000000+(inl(RTC_ADDR)-bootime_lo)/2;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
