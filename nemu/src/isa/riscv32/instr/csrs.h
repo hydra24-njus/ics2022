f0dd40ef143770c02079fdaa949460d223cd8191
tracer-ics2021 <tracer@njuics.org>
2021-11-27 22:24:00 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: b60fc9695fc04614a8ae82fbc7d16f07   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  22:24:00 up  1:30,  1 user,  load average: 0.46, 0.51, 0.49

--- nemu/src/isa/riscv32/instr/csrs.h
+++ nemu/src/isa/riscv32/instr/csrs.h
@@ -8,7 +8,16 @@ def_EHelper(csrrw){
       case 0x342:/*mcause*/t=s->isa.mcause;s->isa.mcause=*id_src1->preg;*ddest=t;break;
       break;
   }
-
+}
+def_EHelper(csrrs){
+    uint32_t t;
+  switch(id_src2->imm){
+      case 0x300:/*mstatus*/t=s->isa.mstatus;s->isa.mstatus=*id_src1->preg|t;*ddest=t;break;
+      case 0x305:/*mtvec*/t=s->isa.mtvec;s->isa.mtvec=*id_src1->preg|t;*ddest=t;break;
+      case 0x341:/*mepc*/t=s->isa.mepc;s->isa.mepc=*id_src1->preg|t;*ddest=t;break;
+      case 0x342:/*mcause*/t=s->isa.mcause;s->isa.mcause=*id_src1->preg|t;*ddest=t;break;
+      break;
+  }
 }
 def_EHelper(ecall){
   s->isa.mepc=cpu.pc;
