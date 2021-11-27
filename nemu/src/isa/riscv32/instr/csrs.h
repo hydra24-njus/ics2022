ba7c917876d90530783020dd54670de4d4da7c2f
tracer-ics2021 <tracer@njuics.org>
2021-11-27 21:34:02 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: b60fc9695fc04614a8ae82fbc7d16f07   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  21:34:02 up 40 min,  1 user,  load average: 0.32, 0.52, 0.53

--- nemu/src/isa/riscv32/instr/csrs.h
+++ nemu/src/isa/riscv32/instr/csrs.h
@@ -1,6 +1,13 @@
 #include "cpu/exec.h"
 def_EHelper(csrrw){
-  
+    uint32_t t;
+  switch(id_src2->imm){
+      case 0x300:/*mstatus*/t=cpu.mstatus;cpu.mstatus=*id_src1->preg;*ddest=t;break;
+      case 0x305:/*mtvec*/t=cpu.mtvec;cpu.mtvec=*id_src1->preg;*ddest=t;break;
+      case 0x341:/*mepc*/t=cpu.mepc;cpu.mepc=*id_src1->preg;*ddest=t;break;
+      case 0x342:/*mcause*/t=cpu.mcause;cpu.mcause=*id_src1->preg;*ddest=t;break;
+  }
+
 }
 def_EHelper(ecall){
   
