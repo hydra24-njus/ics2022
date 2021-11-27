8a09f61f14daab6c537fa0e28ee92d79aa83396c
tracer-ics2021 <tracer@njuics.org>
2021-11-27 21:59:19 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: b60fc9695fc04614a8ae82fbc7d16f07   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  21:59:20 up  1:05,  1 user,  load average: 0.33, 0.39, 0.46

--- nemu/src/isa/riscv32/instr/csrs.h
+++ nemu/src/isa/riscv32/instr/csrs.h
@@ -2,10 +2,10 @@
 def_EHelper(csrrw){
     uint32_t t;
   switch(id_src2->imm){
-      case 0x300://*mstatus*/t=isa.mstatus;isa.mstatus=*id_src1->preg;*ddest=t;break;
-      case 0x305:/*mtvec*/t=s->isa.mtvec;s->isa.mtvec=*id_src1->preg;*ddest=t;*id_src1->preg=1;printf("%u\n",s->isa.mtvec);break;
-      case 0x341://*mepc*/t=isa.mepc;isa.mepc=*id_src1->preg;*ddest=t;break;
-      case 0x342:///*mcause*/t=isa.mcause;isa.mcause=*id_src1->preg;*ddest=t;break;
+      case 0x300:/*mstatus*/t=s->isa.mstatus;s->isa.mstatus=*id_src1->preg;*ddest=t;break;
+      case 0x305:/*mtvec*/t=s->isa.mtvec;s->isa.mtvec=*id_src1->preg;*ddest=t;break;
+      case 0x341:/*mepc*/t=s->isa.mepc;s->isa.mepc=*id_src1->preg;*ddest=t;break;
+      case 0x342:/*mcause*/t=s->isa.mcause;s->isa.mcause=*id_src1->preg;*ddest=t;break;
       break;
   }
 
