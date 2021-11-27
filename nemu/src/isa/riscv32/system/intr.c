d2ea8116450e2f48a6dc872833123eecef7fd89c
tracer-ics2021 <tracer@njuics.org>
2021-11-27 22:10:07 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: b60fc9695fc04614a8ae82fbc7d16f07   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  22:10:07 up  1:16,  1 user,  load average: 0.23, 0.38, 0.43

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -5,7 +5,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  s->isa.mepc=epc;
+  //s->isa.mepc=epc;
 
   return 0;
 }
