c171d2e22af6ae88f1c0f0e4b1da7e01c2f41658
tracer-ics2021 <tracer@njuics.org>
2021-11-27 19:10:48 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 8ac5975ce38044819eb48ca265257b38   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:10:48 up 57 min,  1 user,  load average: 0.72, 0.67, 0.60

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,81 +4,133 @@
 #include <stdarg.h>
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int i2a(int n,int base,char tmp2[15]){
-	int x=0;
-	while(n!=0){
-		tmp2[x++]=n%10+'0';
-		n/=10;
-	}
-	return x;
-}
+
 //使用参数列表fmt发送格式化输出到out
-int vsprintf(char *out, const char *fmt, va_list ap) {
-	int len;
-	const char *t;
-	char *tmp = out;
-	for (; *fmt; fmt++){
-		if (*fmt != '%'){
-			*tmp++ = *fmt;
-			continue;
-		}
-		fmt++;
-		switch (*fmt){
-		case 's':{
-			t = va_arg(ap, char *);
-			len = strlen(t);
-			for (int i = 0; i < len; i++){
-				*tmp++ = *t++;
-			}
-			break;
-		}
-		case 'd':{
-			int n = va_arg(ap, int);
-			if (n == 0){
-				*tmp++ = '0';
-				break;
-			}
-			else if (n < 0){
-				*tmp++ = '-';
-				n = -n;
-			}
-			char num[15];
-			int x=i2a(n,10,num);
-			while(x-->0){
-				*tmp++=num[x];
-			}
-			break;
-		}
-		default:break;
+struct param {
+	char uc:1;
+	char sign;			/* '-' 				 */
+	unsigned int base;	/* number base, e.g 8, 10, 16*/
+	char *bf;			/* buffer for output */
+};
+typedef void (*putcf) (void *, char);
+
+struct _vsprintf_putcf_data {
+	char *dest;
+	size_t num_chars;
+};
+
+static void _vsprintf_putcf(void *p, char c) {
+	struct _vsprintf_putcf_data *data = (struct _vsprintf_putcf_data *)p;
+	data->dest[data->num_chars ++] = c;
+}
+
+static void _printf_putc(void *p, char c) {  
+	putch(c);
+}
+
+static void putchw(void *putp, putcf putf, struct param *p) {
+	char ch;
+	char *bf = p->bf;
+	while((ch = *bf++)) 
+		putf(putp, ch);
+}
+
+static void ui2a(unsigned int num, struct param *p) {
+	int n = 0;
+	unsigned int d = 1;
+	char *bf = p->bf;
+	while (num / d >= p->base)
+		d = d * p->base;
+
+	while (d != 0) {
+		int dgt = num /d;
+		num = num % d;
+		d = d / p->base;
+		if (n || dgt > 0 || d == 0) {
+			*bf++ = dgt + (dgt < 10 ? '0' : (p->uc ? 'A' : 'a') - 10);
+			++n;
 		}
 	}
-	*tmp = '\0';
-	return 0;
+	*bf = 0;
+}
+static void i2a (int num, struct param *p) {
+	//printf("num = %d\n", num);
+	if (num < 0) {
+		num = -num;
+		p->sign = '-';
+	}
+	ui2a(num, p);
+}
+
+
+void my_format(void *putp, putcf putf, const char *fmt, va_list va) {
+	struct param p;
+	char bf[12];	/* int = 32b */
+	char ch;
+	p.bf = bf;
+
+	while ((ch = *(fmt++))) {
+		if (ch != '%') {
+			putf(putp, ch);
+		} else {
+			ch = *(fmt++);
+			switch (ch) {
+				case 'd':
+					p.base = 10;
+					p.uc = 0;
+					i2a(va_arg(va, int), &p);
+					putchw(putp, putf, &p);
+					break;
+				case 'x' :
+					p.base = 16;
+					p.uc = 0;
+					i2a(va_arg(va, int), &p);
+					putchw(putp, putf, &p);
+					break;
+				case 's':
+					p.bf = va_arg(va, char *);
+					putchw(putp, putf, &p);
+				    p.bf = bf; //???
+					break;
+				default : break;
+			}
+		}
+
+	}
+}
+int vsprintf(char *out, const char *fmt, va_list ap) {
+	struct _vsprintf_putcf_data data;
+	data.dest = out;
+	data.num_chars = 0;
+	my_format(&data, _vsprintf_putcf, fmt, ap);
+	data.dest[data.num_chars] = '\0';
+
+	return data.num_chars;
+}
+static inline void _putstr(char *s) {
 }
 //利用sprintf返回的out字符串进行输出
-int printf(const char *fmt, ...) {
-	
-	char out[1024];//上限1024个字符（应该够吧
-	int len=0;
-	va_list ap;
-	va_start(ap,fmt);
-	len=vsprintf(out,fmt,ap);
-	va_end(ap);
-	int i=0;
-	while(out[i]!='\0'){
-		putch(out[i++]);
-	}
-	return len;
-}
-//发送指定内容到out所指的字符串
 int sprintf(char *out, const char *fmt, ...) {
-    va_list args;
-	int len=0;
-	//这里对参数进行解构，将输出存入out在sprintf中完成。
-	va_start(args, fmt);
-	len = vsprintf(out, fmt, args);//to be completed
-	va_end(args);
-	return len;
+	va_list ap;
+	int ret;
+	
+	va_start(ap, fmt);
+	ret = vsprintf(out, fmt, ap);
+	va_end(ap);
+
+	return ret;
+}
+int printf(const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	my_format(NULL, _printf_putc, fmt, ap);
+	va_end(ap);
+
+	/*
+		0x3F8 
+		printf -> _putc(ch) -> putchar(0x3F8, ch) -> outb(addr, ch) -> *addr = ch
+	 */
+	return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
