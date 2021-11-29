03efc2f4cd556ef6758a3a9a1bbbed8609a63258
tracer-ics2021 <tracer@njuics.org>
2021-11-29 18:36:23 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: bcda335fded24fca9b469fb4df84a33d   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  18:36:24 up 42 min,  1 user,  load average: 0.84, 0.71, 0.51

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,6 +14,7 @@ extern size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   Elf_Ehdr Ehdr;
+  printf("%d\n",get_ramdisk_size());
   ramdisk_read(&Ehdr, 0, get_ramdisk_size());
   return Ehdr.e_entry;
   //return 0;
