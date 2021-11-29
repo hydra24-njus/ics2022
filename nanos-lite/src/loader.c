9026a11b8be8cc81a07583f46866e7495f731eb0
tracer-ics2021 <tracer@njuics.org>
2021-11-29 23:29:01 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 919c0c18bb3448519a0547a47ec799a9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  23:29:01 up 21 min,  1 user,  load average: 0.05, 0.18, 0.26

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,7 +24,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
+  uintptr_t entry = 0x800003dc;loader(pcb, filename);
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
