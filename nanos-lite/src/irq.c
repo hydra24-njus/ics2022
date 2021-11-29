f4634d06cc421fa97033e95506076819ad114e40
tracer-ics2021 <tracer@njuics.org>
2021-11-29 15:42:03 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 8a5b7c04651548ba8c4cc3e30382c3ba   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  15:42:03 up  1:48,  1 user,  load average: 0.55, 0.55, 0.50

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,15 +1,9 @@
 #include <common.h>
 
 static Context* do_event(Event e, Context* c) {
-	uint32_t i=0;
   switch (e.event) {
     case EVENT_YIELD:
     	printf("yield!\n");
-    	asm volatile("csrrs %0,mepc, %0;"
-    		     "addi  %0,%0,0x4;"
-    		     "csrrs %0,mepc, %0;"
-    	 : :"r"(i)
-    	 );
     	break;
     default: panic("Unhandled event ID = %d", e.event);
   }
