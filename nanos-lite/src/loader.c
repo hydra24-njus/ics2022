6b64b795b81c796c0d95297ab46e4a6184a4dff0
tracer-ics2021 <tracer@njuics.org>
2021-11-29 20:45:45 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 9ee61c417a9d46ce95cb207e13ef1299   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  20:45:45 up 7 min,  1 user,  load average: 0.37, 0.70, 0.44

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,25 +7,24 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
-
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
 extern size_t ramdisk_read(void *, size_t, size_t);
 extern size_t ramdisk_write(const void*, size_t, size_t);
 extern size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
+
   Elf_Ehdr eh;
   Elf_Phdr ph;
   ramdisk_read(&eh, 0, sizeof(Elf_Ehdr));
-  printf("%d\t%x\n",get_ramdisk_size(),&eh.e_entry);
   for (int i = 0; i < eh.e_phnum; i++) {
     ramdisk_read(&ph, eh.e_phoff + i * sizeof(Elf_Phdr), sizeof(Elf_Phdr));
     if (ph.p_type == 1) {
       ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
-      memset((void *)ph.p_vaddr + ph.p_filesz, 0, ph.p_memsz - ph.p_filesz);
     }
   }
   return eh.e_entry;
-
   //return 0;
 }
 
