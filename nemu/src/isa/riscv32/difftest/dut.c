7ccacdedf75c35c040d5e18a721c057a2882935a
tracer-ics2021 <tracer@njuics.org>
2021-11-30 19:45:11 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: ce27c115f79c4beb93f7f319ab8d3f05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:45:11 up  6:16,  1 user,  load average: 0.86, 0.93, 0.73

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,8 +3,8 @@
 #include "../local-include/reg.h"
 extern const char *regs[];
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  printf("%x,%x,%x,%x\n",ref_r->mepc,ref_r->mcause,ref_r->mstatus,ref_r->mtvec);
-  printf("%x,%x,%x,%x\n",cpu.mepc,cpu.mcause,cpu.mstatus,cpu.mtvec);
+  //printf("%x,%x,%x,%x\n",ref_r->mepc,ref_r->mcause,ref_r->mstatus,ref_r->mtvec);
+  //printf("%x,%x,%x,%x\n",cpu.mepc,cpu.mcause,cpu.mstatus,cpu.mtvec);
   for (int i = 0; i < 32; i++) {
     /*if (ref_r->gpr[i]._32 != gpr(i)) {
       printf("%s.ref=%-15u%s.nemu=%u\n",regs[i],ref_r->gpr[i]._32,regs[i],gpr(i));
