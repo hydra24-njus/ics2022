67f821613a63c32f31a82d3bb86e27637ff4b74f
tracer-ics2021 <tracer@njuics.org>
2021-11-30 16:09:02 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: ce27c115f79c4beb93f7f319ab8d3f05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:09:02 up  2:40,  1 user,  load average: 0.63, 0.47, 0.38

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,9 @@
 #include <common.h>
-
+extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:printf("yield!\n");break;
-    case EVENT_SYSCALL:printf("syscall!\n");break;
+    case EVENT_SYSCALL:do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
