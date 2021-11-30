2ab5ec3906d9d653dd3cc79bf403148f77a73d10
tracer-ics2021 <tracer@njuics.org>
2021-11-30 15:51:17 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: ce27c115f79c4beb93f7f319ab8d3f05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  15:51:17 up  2:22,  1 user,  load average: 0.35, 0.48, 0.25

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,8 +7,10 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+    printf("%d\n",c->mcause);
     switch (c->mcause) {
       case -1:ev.event=EVENT_YIELD;c->mepc+=4;break;
+
       default: ev.event = EVENT_ERROR; break;
     }
     c = user_handler(ev, c);
