7c7f29d465b0c27bebaf3a79f58612c0d3ff9a1c
tracer-ics2021 <tracer@njuics.org>
2021-11-30 19:51:30 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: ce27c115f79c4beb93f7f319ab8d3f05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:51:30 up  6:22,  1 user,  load average: 0.38, 0.50, 0.60

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@ extern void do_syscall(Context*);
 static Context* do_event(Event e, Context* c) {
 printf("%x\n",e.event);
 printf("%x\n",c->GPR1);
-  e.event=c->GPR1;
+  //e.event=c->GPR1;
 
   switch (e.event) {
     case EVENT_YIELD:printf("yield!\n");break;
