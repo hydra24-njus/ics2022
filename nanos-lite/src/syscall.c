3820f51070884154fd9c8c1d93575e8eae2c3244
tracer-ics2021 <tracer@njuics.org>
2021-11-30 16:18:11 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: ce27c115f79c4beb93f7f319ab8d3f05   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:18:11 up  2:49,  1 user,  load average: 0.58, 0.56, 0.44

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,6 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  printf("%p\n",a[0]);
   switch (a[0]) {
     case 1:yield();c->GPRx=0;
     default: panic("Unhandled syscall ID = %d", a[0]);
