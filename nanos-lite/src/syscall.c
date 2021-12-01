c9b4ffcafa21fbd4796fa5b97654c8f94085304d
tracer-ics2021 <tracer@njuics.org>
2021-12-01 12:31:10 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: afbe02a2885d4339b1352c38f0e369c9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  12:31:11 up 54 min,  1 user,  load average: 0.44, 0.53, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,27 +8,18 @@ int sys_yield(){
 void sys_exit(uintptr_t ret){
   halt(ret);
 }
-int do_write(int fd, const void*buf, size_t count){   
-    if(fd==1 || fd==2){
-        for(int i = 0;i < count;i++){
-            putch(((char*)buf)[i]);
-        }
-        return count;
-    }
-    if(fd==0)return 0;
-  return 0;
-}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  //printf("%u\n",a[0]);
+  printf("%u\n",a[0]);
   switch (a[0]) {
     case SYS_exit:printf("%d\n",a[1]);sys_exit(a[1]);break;
     case SYS_yield:c->GPRx=sys_yield();break;
-    case SYS_write:c->GPRx = do_write(a[1], (void*)(a[2]), a[3]);break;
+    case SYS_write:printf("sys_write\n");break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
