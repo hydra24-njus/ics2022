fe32ce2051d7ef8c86755c9621120ce4a5b7fbc4
tracer-ics2021 <tracer@njuics.org>
2021-12-02 14:39:13 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: b87334658b7645e9a05d2ac4f90f564f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:39:13 up 26 min,  1 user,  load average: 0.21, 0.40, 0.30

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -10,31 +10,14 @@ int main() {
   int sec = 1;
   while(1)
   {
-    while(num.tv_sec <= sec)
+    while(num.tv_sec <= sec||num.tv_usec<=500000)
     {
       int t = gettimeofday(&num,NULL);
       
       assert(t==0);
     }
     
-    printf("time: seconds = %d   sec=%d usec%d\n",sec,num.tv_sec,num.tv_usec);
+    printf("time: seconds = %d   sec=%d usec=%d\n",sec,num.tv_sec,num.tv_usec);
     sec++;
   } 
 }
-
-/* int t=0;
-  printf("time-test starts\n");
-  int sec = 1;
-  //NDL_Init(1);
-  while(1)
-  {
-    while(t <= sec)
-    {
-      t = NDL_GetTicks()/1000000;
-    }
-      printf("?\n");
-
-    printf("time: seconds = %d\n",sec);
-    sec++;
-  } 
-  NDL_Quit(); */
