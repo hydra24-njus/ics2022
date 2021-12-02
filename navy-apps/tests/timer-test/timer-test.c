b85fabf22d18c0414157ae19895541c636b68909
tracer-ics2021 <tracer@njuics.org>
2021-12-02 20:01:00 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 4950f00b3dc6491d9e1f142a7c8d9110   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  20:01:00 up  1:53,  1 user,  load average: 0.17, 0.33, 0.37

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -4,16 +4,13 @@ int main() {
   struct timeval dut; 
   dut.tv_sec = 0;dut.tv_usec = 0;
   struct timeval ref;
-  ref.tv_sec = 1;ref.tv_usec = 500000;
+  ref.tv_sec = 1;ref.tv_usec = 0;
   printf("time-test starts begin = %ld\n",dut.tv_sec);
-  do
-  {
+  while(1){
     gettimeofday(&dut,NULL);
     if(dut.tv_sec==ref.tv_sec){
-      switch(ref.tv_usec){
-      	case      0:if(dut.tv_usec>ref.tv_usec)printf("0.0s\n");ref.tv_usec=500000;break;
-      	case 500000:if(dut.tv_usec>ref.tv_usec)printf("0.5s\n");ref.tv_usec=0;ref.tv_sec++;break;
-      }
+      printf("sec:%d  usec:%d\n",ref.tv_sec,ref.tv_usec);
+      ref.tv_sec++;
     }
-  } while(1);
+  }
 }
