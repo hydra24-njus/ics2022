8d882883f5556d580e452794c319afa2cdfb7f0c
tracer-ics2021 <tracer@njuics.org>
2021-12-02 13:16:18 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 4d1742fbe82b4039a6c2d579d58af3d5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  13:16:18 up  1:27,  1 user,  load average: 0.57, 0.35, 0.24

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -15,7 +15,6 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-printf("success!\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
