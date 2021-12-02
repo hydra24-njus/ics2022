7d7311d334800375ea56b71ee0d2574f2fdd40f8
tracer-ics2021 <tracer@njuics.org>
2021-12-02 12:42:34 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 4d1742fbe82b4039a6c2d579d58af3d5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  12:42:34 up 53 min,  1 user,  load average: 0.28, 0.29, 0.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,11 @@
 #include <common.h>
 #include "syscall.h"
 
+extern int fs_open(const char */*, int flags, mode_t mode*/);
+extern size_t fs_lseek(int fd, size_t offset, int whence);
+extern size_t fs_read(int,void *,size_t);
+extern size_t fs_write(int,const void*,size_t);
+extern int fs_close(int);
 int sys_yield(){
   yield();
   return 0;
@@ -8,16 +13,25 @@ int sys_yield(){
 void sys_exit(uintptr_t ret){
   halt(ret);
 }
-
+int sys_open(const char *pathname){
+  int res=fs_open(pathname);
+  return res;
+}
+int sys_read(int fd, void*buf, size_t count){
+  int res = fs_read(fd, buf, count);
+  return res;
+}
 int sys_write(int fd,const void *buf, size_t count){
-  if(fd==1||fd==2){
-    for(int i=0;i<count;i++){
-      putch(((char*)buf)[i]);
-    }
-    return count;
-  }
-  else if(fd==0)return 0;
-  return 0;
+  int res = fs_write(fd, buf, count);
+  return res;
+}
+size_t sys_lseek(int fd, size_t offset, int whence){
+  size_t res = fs_lseek(fd, offset, whence);
+  return res;
+}
+int sys_close(int fd){
+  int res = fs_close(fd);
+  return res;
 }
 //static intptr_t brk;
 int sys_brk(int addr){
@@ -34,7 +48,11 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_exit:printf("exit\n");sys_exit(a[1]);break;
     case SYS_yield:c->GPRx=sys_yield();break;
+    case SYS_open:c->GPRx=sys_open((const char*)(a[1]));break;
+    case SYS_lseek:c->GPRx=sys_lseek(a[1],a[2],a[3]);break;
+    case SYS_read:c->GPRx=sys_read(a[1],(void*)(a[2]),a[3]);break;
     case SYS_write:c->GPRx=sys_write(a[1],(void*)a[2],a[3]);break;
+    case SYS_close:c->GPRx=sys_close(a[1]);break;
     case SYS_brk:c->GPRx=sys_brk(a[1]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
