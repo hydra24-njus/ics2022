a3084661c345400e153a843b86d93de05d218492
tracer-ics2021 <tracer@njuics.org>
2021-12-02 14:45:59 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: b87334658b7645e9a05d2ac4f90f564f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:45:59 up 33 min,  1 user,  load average: 0.70, 0.56, 0.40

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -17,8 +17,8 @@ void do_syscall(Context *c) {
     case SYS_yield:yield();break;
     case SYS_open:c->GPRx=fs_open((const char*)(a[1]));break;
     case SYS_lseek:c->GPRx=fs_lseek(a[1],a[2],a[3]);break;
-    case SYS_read:Log("sys_call:read");c->GPRx=fs_read(a[1],(void*)(a[2]),a[3]);break;
-    case SYS_write:Log("sys_write");c->GPRx=fs_write(a[1],(void*)a[2],a[3]);break;
+    case SYS_read:/*Log("sys_call:read");*/c->GPRx=fs_read(a[1],(void*)(a[2]),a[3]);break;
+    case SYS_write:/*Log("sys_write");*/c->GPRx=fs_write(a[1],(void*)a[2],a[3]);break;
     case SYS_close:c->GPRx=fs_close(a[1]);break;
     case SYS_brk:c->GPRx=0;break;
     case SYS_gettimeofday:break;
