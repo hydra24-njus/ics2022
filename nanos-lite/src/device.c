c6939f1bfa24340acfdf32b5f91123de9fdbba70
tracer-ics2021 <tracer@njuics.org>
2021-12-03 14:01:24 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: f7564345b4e647948d909d5de316d8c6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:01:24 up  1:54,  1 user,  load average: 0.41, 0.39, 0.28

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -30,7 +30,7 @@ int sys_gettimeofday(struct timeval *tv){
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if(ev.keycode==AM_KEY_NONE)return 0;
-  sprintf(buf,"%s",keyname[ev.keycode]);
+  sprintf(buf,"%s\t%s",keyname[ev.keycode],ev.keydown?"down":"up");
   return len;
 }
 
