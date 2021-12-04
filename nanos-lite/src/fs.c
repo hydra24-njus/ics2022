45e15f2a5e3cf979e999e3d38bc050293bf266de
tracer-ics2021 <tracer@njuics.org>
2021-12-04 13:11:55 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a87251861c9a445e98605ab1c8b653be   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  13:11:56 up 31 min,  1 user,  load average: 0.54, 0.42, 0.41

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -7,7 +7,7 @@ typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 extern size_t serial_write(const void*, size_t, size_t);
 extern size_t events_read(void *buf, size_t offset, size_t len);
 extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
-
+extern size_t fb_write(const void *buf, size_t offset, size_t len);
 
 typedef struct {
   char *name;
@@ -18,7 +18,7 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENT, FD_FB};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENT, FD_DISPINFO, FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -36,7 +36,8 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
   [FD_EVENT]  = {"/dev/events", 0, 0, 0, events_read, invalid_write},
-  [FD_FB]     = {"/dev/fb", 0, 0, 0, dispinfo_read, invalid_write},
+  [FD_DISPINFO] = {"/proc/dispinfo", 64, 0, 0, dispinfo_read,invalid_write},
+  [FD_FB]     = {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
 #include "files.h"
 };
 
