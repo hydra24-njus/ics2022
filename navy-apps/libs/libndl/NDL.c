64ad5a1932b01a6cc0c50d8e946f9741806aca45
tracer-ics2021 <tracer@njuics.org>
2021-12-04 15:36:25 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a87251861c9a445e98605ab1c8b653be   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:36:25 up  2:55,  1 user,  load average: 0.23, 0.24, 0.26

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -56,7 +56,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 static uint32_t* canvas =NULL;
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = _open("/dev/fb", 0, 0);
-  for (int i = 0; i < h; i++) {
+  for (int i = 0; i < 128; i++) {
     lseek(fd, ((y + i) * screen_w + x) * 4, 0);
     write(fd, pixels + i * w, w * 4);
   }
