36aa9fe470eee0233892cd0b5930adb5510f3276
tracer-ics2021 <tracer@njuics.org>
2021-12-04 15:03:12 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a87251861c9a445e98605ab1c8b653be   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:03:12 up  2:22,  1 user,  load average: 0.10, 0.22, 0.26

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -8,9 +8,10 @@ int main() {
   NDL_Init(0);
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  printf("%d\t%d\n",w,h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
-  printf("%d\t%d\n",w,h);
+  
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
