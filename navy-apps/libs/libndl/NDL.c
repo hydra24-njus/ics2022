949aefb766cf4227c50b703c701352ef6cdea7be
tracer-ics2021 <tracer@njuics.org>
2021-12-04 13:34:10 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a87251861c9a445e98605ab1c8b653be   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  13:34:10 up 53 min,  1 user,  load average: 0.32, 0.35, 0.33

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -44,12 +44,7 @@ void NDL_OpenCanvas(int *w, int *h) {
     _read(4,buf,64);
     //printf("%s\n",buf);
     sscanf(buf, "WIDTH: %d\nHEIGHT: %d\n", w, h);
-    printf("%d\t%d\n",*w,*h);
-    /*for(int i=0;i<64;i++)printf("%c",buf[i]);
-    int i=6;
-    for(i=7;buf[i]!='\n';i++)printf("%c ",buf[i]);
-    i+=7;printf("\n");
-    for(i;buf[i]!='\n';i++)printf("%c ",buf[i]);*/
+    //printf("%d\t%d\n",*w,*h);
   }
 }
 
