55dd2299c9c6eddbffaea7e0b8578a647a32be19
tracer-ics2021 <tracer@njuics.org>
2021-12-04 15:32:34 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a87251861c9a445e98605ab1c8b653be   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:32:34 up  2:51,  1 user,  load average: 0.25, 0.24, 0.25

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -57,7 +57,7 @@ static uint32_t* canvas =NULL;
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = _open("/dev/fb", 0, 0);
   for (int i = 0; i < h; i++) {
-    lseek(fd, 0, ((y + i) * screen_w + x) * 4);
+    lseek(fd, 0, ((y + i) * canvas_w + x) * 4);
     write(fd, pixels + i * w, w * 4);
   }
 }
--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -9,7 +9,7 @@ int main() {
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
-  int x=0;
+  //int x=0;
   NDL_OpenCanvas(&w, &h);
   //NDL_OpenCanvas(&x, &x);
   NDL_DrawRect(bmp, 0, 0, w, h);
