086e6e318f052627c11f48d892a7c3eca9be8783
tracer-ics2021 <tracer@njuics.org>
2021-12-04 15:12:30 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a87251861c9a445e98605ab1c8b653be   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:12:30 up  2:31,  1 user,  load average: 0.42, 0.28, 0.27

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -7,8 +7,7 @@
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
-static int canvas_w=0;canvas_h=0;
-
+static int canvas_w = 0, canvas_h = 0;
 uint32_t NDL_GetTicks() {
   struct timeval tv;
   struct timezone tz;
@@ -44,7 +43,7 @@ void NDL_OpenCanvas(int *w, int *h) {
     if(*w==0&&*h==0){
       char buf[64];
       _read(4,buf,64);
-      sscanf("WIDTH:%d\nHEIGHT:%d\n",&canvas_w,&canvas_h);
+      sscanf(buf,"WIDTH:%d\nHEIGHT:%d\n",&canvas_w,&canvas_h);
     }
     else{
       canvas_w=*w,canvas_h=*h;
