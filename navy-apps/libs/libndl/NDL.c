34a63555fdb9b5a52f8955145e9261cfc4bd4f31
tracer-ics2021 <tracer@njuics.org>
2021-12-04 15:36:13 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a87251861c9a445e98605ab1c8b653be   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:36:13 up  2:55,  1 user,  load average: 0.18, 0.23, 0.25

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -56,13 +56,10 @@ void NDL_OpenCanvas(int *w, int *h) {
 static uint32_t* canvas =NULL;
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = _open("/dev/fb", 0, 0);
-  int i=0;
-  //for (int i = 0; i < h; i++) {
-    lseek(fd, 0, ((y + i) * screen_w + x) * 4);
+  for (int i = 0; i < h; i++) {
+    lseek(fd, ((y + i) * screen_w + x) * 4, 0);
     write(fd, pixels + i * w, w * 4);
-    lseek(fd,((y + 1) * screen_w + x) * 4,0);
-    write(fd, pixels + 1 * w, w * 4);
-  //}
+  }
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
