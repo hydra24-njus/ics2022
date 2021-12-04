d2c157d6c74851da09e241eab13eb527f0ba184c
tracer-ics2021 <tracer@njuics.org>
2021-12-04 15:09:36 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: a87251861c9a445e98605ab1c8b653be   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:09:36 up  2:28,  1 user,  load average: 0.19, 0.20, 0.24

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -9,8 +9,8 @@ int main() {
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
-  NDL_OpenCanvas(0, 0);
-  //NDL_DrawRect(bmp, 0, 0, w, h);
+  NDL_OpenCanvas(1, 1);
+  NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
