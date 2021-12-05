716f7aa515d3bc9971cd4eb4d33c9ddf19591769
tracer-ics2021 <tracer@njuics.org>
2021-12-05 22:47:41 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 91a8bacd02724d729059f1ceb3de50b3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  22:47:41 up 35 min,  1 user,  load average: 0.39, 0.31, 0.31

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,60 +1,38 @@
 #include <NDL.h>
 #include <SDL.h>
-#include <string.h>
 
 #define keyname(k) #k,
 
-static const char *keyname[] = {"NONE", _KEYS(keyname)};
-uint8_t keystate[256] = {};
+static const char *keyname[] = {
+  "NONE",
+  _KEYS(keyname)
+};
 
-int SDL_PushEvent(SDL_Event *ev) { return 0; }
+int SDL_PushEvent(SDL_Event *ev) {
+  return 0;
+}
 
 int SDL_PollEvent(SDL_Event *ev) {
-    char buf[64];
-    char state[2];
-    char name[20];
-    NDL_PollEvent(buf, 64);
-    if (buf[0] == 0)
-        return 0;
-    sscanf(buf, "%s %s\n", state, name);
-    ev->key.type = ev->type = (state[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
-    for (int i = 0; i < 83; i++) {
-        if (!strcmp(keyname[i], name)) {
-            ev->key.keysym.sym = i;
-            keystate[i] = (state[1] == 'd') ? 1 : 0;
-            break;
-        }
-    }
-
-    return 1;
+  return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-    while (1) {
-        char buf[64];
-        char state[2];
-        char name[20];
-        NDL_PollEvent(buf, 64);
-        if (buf[0] == 0)
-            continue;
-        sscanf(buf, "%s %s\n", state, name);
-        event->key.type = event->type =
-            (state[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
-        for (int i = 0; i < 83; i++) {
-            if (!strcmp(keyname[i], name)) {
-                event->key.keysym.sym = i;
-                keystate[i] = (state[1] == 'd') ? 1 : 0;
-                break;
-            }
-        }
-        return 1;
-    }
-
-    return 1;
+while(1){
+  char buf[64];
+  NDL_PollEvent(buf, 64);
+  char keydown[3],keycode[15];
+  if(buf[0]=='0')continue;//no event.
+  sscanf(buf,"%s %s\n",keydown,keycode);
+  printf("%s %s\n",keydown,keycode);
+  return 1;
+  }
+  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-    return 0;
+  return 0;
 }
 
-uint8_t *SDL_GetKeyState(int *numkeys) { return keystate; }
+uint8_t* SDL_GetKeyState(int *numkeys) {
+  return NULL;
+}
--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -10,8 +10,6 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst,
                      SDL_Rect *dstrect) {
     assert(dst && src);
     assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-    // printf("dd%d ss%d\n", (int)dst, (int)src);
-    // if (dst && src) {
 assert(0);
 }
 
