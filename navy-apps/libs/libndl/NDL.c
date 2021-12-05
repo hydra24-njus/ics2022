d6b87f4e935bae4aa798a7bf365c3e0c133ff725
tracer-ics2021 <tracer@njuics.org>
2021-12-05 18:15:23 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: dbfd41e4a12f45fcaf3721d16ce9ef52   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:15:23 up  1:46,  1 user,  load average: 0.66, 0.64, 0.70

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -68,7 +68,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   }*/
   write(fd,pixels,w*h);
   for (int i = 0; i < h; i++) {
-    lseek(fd, ((y + i) * screen_w + x) * 4, SEEK_SET); 
+    //lseek(fd, ((y + i) * screen_w + x) * 4, SEEK_SET); 
     write(fd,pixels + i * w, w * 4);
   }
 }
