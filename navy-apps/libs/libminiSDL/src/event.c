2ffbaa75993d7a49b6fbfce6e60b46405825f85b
tracer-ics2021 <tracer@njuics.org>
2021-12-07 23:38:25 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: 889dd07147504d39b5d0709e288e9786   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:38:25 up 44 min,  1 user,  load average: 0.48, 0.47, 0.45

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -17,7 +17,7 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-while(1){
+while(1){//wait
   char buf[64];
   NDL_PollEvent(buf, 64);
   char keydown[3],keycode[15];
