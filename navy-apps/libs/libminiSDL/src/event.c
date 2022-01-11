a5324d058f32f38bee665f6311e07dbc8767fc10
tracer-ics2021 <tracer@njuics.org>
2022-01-11 21:57:50 +0800

    >  compile 201240105    Static hostname: hydra24--R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 7e8b9ac66ec646ddb185d36e1a8f9d16            Boot ID: b95eb4c3a0bc491e85279bdd2ea8858f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:57:50 up 20 min,  1 user,  load average: 0.20, 0.64, 0.52

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -17,20 +17,30 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-while(1){//wait
-  char buf[64];
-  NDL_PollEvent(buf, 64);
-  char keydown[3],keycode[15];
-  if(buf[0]=='0')continue;//no event.
-  sscanf(buf,"%s %s\n",keydown,keycode);
-  //printf("%s %s\n",keydown,keycode);
-  event->key.type=event->type=(keydown[1] == 'd')?SDL_KEYDOWN:SDL_KEYUP;
-  for (int i = 0; i < 83; i++) {
-    if (!strcmp(keyname[i], keycode)) {
-      event->key.keysym.sym = i;
-    }
+char key[64]={0};
+  memset(key,0,sizeof(key));
+  key[0] = '0';
+  int keycode = 0;
+  while(!NDL_PollEvent(key,sizeof(key)));
+  //printf("%s\n",key);
+  char* str = key+3;
+  for(int i = 0,cnt = 0;i < sizeof(keyname);i+=sizeof(keyname[cnt]),cnt+=1)
+  {
+    if(!strncmp(keyname[cnt],str,strlen(str))&&!strncmp(keyname[cnt],str,strlen(keyname[cnt]))) {
+        keycode = cnt;
+        break;
+      }
+    assert(i <= sizeof(keyname));
   }
-  return 1;
+  //printf("in waitevent\n");
+  if(key[1] == 'u')
+  {
+    event->key.keysym.sym = keycode;
+    event->type = SDL_KEYUP;
+  }
+  else if(key[1] == 'd'){
+    event->key.keysym.sym = keycode;
+    event->type = SDL_KEYDOWN;
   }
   return 0;
 }
