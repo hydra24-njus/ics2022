6e9c7f57d4432105a2e0db98fe23b04dbc1059f4
tracer-ics2021 <tracer@njuics.org>
2022-01-13 15:54:54 +0800

    >  compile 201240105    Static hostname: hydra24-R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b2e698dbc014bdda7e1dd7414d78301            Boot ID: 1560174245884ca3963ec2e84b3b27a6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  15:54:54 up 24 min,  1 user,  load average: 0.22, 0.41, 0.27

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -61,7 +61,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     for(int i=0;i<h;i++)
       for(int j=0;j<w;j++)
         pixelbuf[i*w+j]=colors[src[(i+y)*s->w+j+x]].val;
-    ConvertPixelsARGB_ABGR(pixelbuf, pixelbuf, w * h);
+    //ConvertPixelsARGB_ABGR(pixelbuf, pixelbuf, w * h);
     NDL_DrawRect(pixelbuf, x, y, w, h);
     free(pixelbuf);
   }
