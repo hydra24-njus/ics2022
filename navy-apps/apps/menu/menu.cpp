e9828066f41b4b886baf6e7a7c5d67f83a05151b
tracer-ics2021 <tracer@njuics.org>
2022-01-13 17:43:28 +0800

    >  compile 201240105    Static hostname: hydra24-R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b2e698dbc014bdda7e1dd7414d78301            Boot ID: 1560174245884ca3963ec2e84b3b27a6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  17:43:28 up  2:13,  1 user,  load average: 0.78, 0.48, 0.41

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -83,7 +83,7 @@ int main(int argc, char *argv[], char *envp[]) {
     do {
       SDL_WaitEvent(&e);
     } while (e.type != SDL_KEYDOWN);
-    Log("finish");
+print("000\n\n\n");
     int i = -1;
     switch (e.key.keysym.sym) {
       case SDLK_0: i = 0; break;
