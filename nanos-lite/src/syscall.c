ef0ee9ffa0b48fa4ade5293fdb2111830fd8a4d2
tracer-ics2021 <tracer@njuics.org>
2022-01-13 17:06:46 +0800

    >  compile 201240105    Static hostname: hydra24-R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 3b2e698dbc014bdda7e1dd7414d78301            Boot ID: 1560174245884ca3963ec2e84b3b27a6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-16-generic       Architecture: x86-64  17:06:46 up  1:36,  1 user,  load average: 0.22, 0.49, 0.51

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,7 +23,7 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   switch (a[0]) {
-    case SYS_exit:naive_uload(NULL, "/bin/menu"); break;
+    case SYS_exit:naive_uload(NULL, "/bin/nterm"); break;
     case SYS_yield:yield();break;
     case SYS_open:c->GPRx=fs_open((const char*)(a[1]));break;
     case SYS_lseek:c->GPRx=fs_lseek(a[1],a[2],a[3]);break;
@@ -32,7 +32,7 @@ void do_syscall(Context *c) {
     case SYS_close:c->GPRx=fs_close(a[1]);break;
     case SYS_brk:c->GPRx=0;break;
     case SYS_gettimeofday:c->GPRx=sys_gettimeofday((void *)a[1]);break;
-    case SYS_execve:printf("execve\n\n");naive_uload(NULL, (void *)a[3]);break;
+    case SYS_execve:printf("execve\n\n");naive_uload(NULL, (void *)a[1]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
