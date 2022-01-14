f202f3f6d94dd941c08569fe80740b5566fa8b02
tracer-ics2021 <tracer@njuics.org>
2022-01-14 19:17:50 +0800

    >  compile 201240105    Static hostname: hydra24-R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 356319c21a6e4d94ba123aa59940c08e            Boot ID: b9bc20565b1c410abecdb9e33fb93fad   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  19:17:50 up  3:49,  1 user,  load average: 2.16, 1.36, 0.82

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -41,7 +41,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  yield();
+  //yield();
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
   int x = (offset/4)%w;
