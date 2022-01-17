aacbdadf8c15a75d41ab0090c4e8069d825e07ce
tracer-ics2021 <tracer@njuics.org>
2022-01-18 01:08:20 +0800

    >  compile 201240105    Static hostname: hydra24-R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 356319c21a6e4d94ba123aa59940c08e            Boot ID: 54723a35c1f34cf98a4cf84923131cda   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  01:08:20 up 36 min,  1 user,  load average: 0.34, 0.72, 0.80

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -61,34 +61,25 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-  //NDL_OpenCanvas(&w,&h);
-  //assert(0);
-  
-  if(s->format->BitsPerPixel == 32)
-    NDL_DrawRect((uint32_t*)s->pixels,x,y,w,h);//32位的颜色,直接使用NDL
+//printf("updaterect\n");
+  if(w==0||h==0)w=s->w;h=s->h;
+  if (x + w > s->w)w = s->w - x;
+  if (y + h > s->h)h = s->h - y;
+  if(s->format->BitsPerPixel == 32)NDL_DrawRect((uint32_t*)s->pixels,x,y,w,h);
   else{
-    uint32_t s_h = s->h,s_w = s->w;
-    if(w == 0||w > s_w) w = s_w;
-    if(h == 0||h > s_h) h = s_h;
-    uint32_t * palette = malloc(sizeof(uint32_t)*w*h);
-    memset(palette,0,sizeof(palette));
-    for(int i = 0;i < h;i++)
-      for(int j = 0;j < w;j++)
-      {
-        uint8_t r = s->format->palette->colors[s->pixels[(i+y)*s_w+j+x]].r;
-        uint8_t g = s->format->palette->colors[s->pixels[(i+y)*s_w+j+x]].g;
-        uint8_t b = s->format->palette->colors[s->pixels[(i+y)*s_w+j+x]].b;
-        palette[i*w+j] = ((r<<16)|(g<<8)|b);
+    uint8_t* pixel=s->pixels;
+    uint32_t* pixelbuf=malloc(sizeof(uint32_t)*w*h);
+    for(int i=0;i<h;i++){
+      for(int j=0;j<w;j++){
+        uint8_t r = s->format->palette->colors[s->pixels[(i+y)*s->w+j+x]].r;
+        uint8_t g = s->format->palette->colors[s->pixels[(i+y)*s->w+j+x]].g;
+        uint8_t b = s->format->palette->colors[s->pixels[(i+y)*s->w+j+x]].b;
+        pixelbuf[i*w+j]=((r<<16)|(g<<8)|b);
       }
-    NDL_DrawRect(palette,x,y,w,h);
-    free(palette);
-  }//pal8位的索引颜色
-    /* printf("%d %d %d %d\n",x,y,w,h);
-    for(int i = 0;i < w;i++)
-      printf("%x\n",palette[i]); */
-    //printf("1\n");
-  //printf("please implement me\n");
-  //assert(0);
+    }
+    NDL_DrawRect(pixelbuf,x,y,w,h);
+    free(pixelbuf);
+  }
 }
 
 // APIs below are already implemented.
