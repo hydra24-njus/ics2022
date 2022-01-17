c6957dcd731270c01a39eeb1f2d74bde4ede4e20
tracer-ics2021 <tracer@njuics.org>
2022-01-18 01:17:15 +0800

    >  compile 201240105    Static hostname: hydra24-R7000P          Icon name: computer-laptop            Chassis: laptop         Machine ID: 356319c21a6e4d94ba123aa59940c08e            Boot ID: 54723a35c1f34cf98a4cf84923131cda   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  01:17:16 up 45 min,  1 user,  load average: 0.84, 0.87, 0.85

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -30,7 +30,6 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst,
     uint32_t width=dst->format->BytesPerPixel;
     for(int i=0;i<hs;i++)
       memcpy(colord+width*((i+yd)*dst->w+xd),colors+width*((i+ys)*src->w+xs),width*ws);
-    SDL_UpdateRect(dst, xd, yd, wd, hd);
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
@@ -58,7 +57,6 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
       for(int j = 0;j < w;j ++)
         ((uint8_t*)dst->pixels)[(i+y)*dst->w+j+x] = (uint8_t)color;
   } 
-  //SDL_UpdateRect(dst, x, y, w, h);
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
